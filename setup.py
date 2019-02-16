6e249ba8e262084b0a529402256c6198b9a76aff
brilam <brian_lam@live.com>
2019-02-16 17:08:37 -0500

    Updated setup.py to reflect version change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
   name = 'removebg',         # How you named your package folder (MyLib)
   packages = ['removebg'],   # Chose the same as "name"
-  version = '0.02',      # Start with a small number and increase it with every change you make
-  license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  version = '0.03',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "A Python API wrapper for removing background using remove.bg's API",   # Give a short description about your library
   author = 'Brian Lam',                   # Type in your name
   author_email = 'brian_lam@live.com',      # Type in your E-Mail
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU Affero General Public License v3',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      # Specify which Python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
