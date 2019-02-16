39a6242d0b78c1f5de9f9c374164396c599e92ea
brilam <brian_lam@live.com>
2019-02-15 20:10:41 -0500

    Update names and packages in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'remove-bg',         # How you named your package folder (MyLib)
-  packages = ['remove-bg'],   # Chose the same as "name"
+  name = 'removebg',         # How you named your package folder (MyLib)
+  packages = ['removebg'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "A Python API wrapper for removing background using remove.bg's API",   # Give a short description about your library
@@ -9,15 +9,15 @@ setup(
   author_email = 'brian_lam@live.com',      # Type in your E-Mail
   url = 'https://github.com/brilam/remove-bg',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/brilam/remove-bg/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests',
+  keywords = ['remove-bg', 'remove', 'background'],   # Keywords that define your package best
+  install_requires=[
+          'requests'
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'GNU General Public License v3.0',   # Again, pick a license
     'Programming Language :: Python :: 3',      # Specify which Python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
