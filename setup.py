ac14030ca5819227ee3a8d7d7114ade9da140f8f
brilam <brian_lam@live.com>
2020-06-06 17:23:22 -0400

    Updated version of remove.bg package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'removebg',         # How you named your package folder (MyLib)
   packages = ['removebg'],   # Chose the same as "name"
-  version = '0.03',      # Start with a small number and increase it with every change you make
+  version = '0.04',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "A Python API wrapper for removing background using remove.bg's API",   # Give a short description about your library
   author = 'Brian Lam',                   # Type in your name
   author_email = 'brian_lam@live.com',      # Type in your E-Mail
   url = 'https://github.com/brilam/remove-bg',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/brilam/remove-bg/archive/v_003.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/brilam/remove-bg/archive/v_004.tar.gz',    # I explain this later on
   keywords = ['remove-bg', 'remove', 'background'],   # Keywords that define your package best
   install_requires=[
           'requests'
